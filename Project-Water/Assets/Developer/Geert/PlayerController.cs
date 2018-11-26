2296235a2a284bb8737f76d65fd9c0cc8920ab2a
Unknown <Tullemans.geert@Gmail.com>
2018-11-26 12:16:17 +0100

    Movement en harpoen Bootjes

---
+++ Project-Water/Assets/Developer/Geert/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XboxCtrlrInput;
+
+public class PlayerController : MonoBehaviour {
+    [SerializeField] private float _Speed;
+
+    private Vector3 _InputDir;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        _InputDir.x = XCI.GetAxisRaw(XboxAxis.LeftStickX);
+        _InputDir.z = XCI.GetAxisRaw(XboxAxis.LeftStickY);
+
+
+        _InputDir=_InputDir.normalized;
+        _InputDir *= Time.deltaTime;
+        _InputDir *= _Speed;
+
+
+
+        transform.LookAt(transform.position + _InputDir);
+        transform.Translate(-_InputDir.z, 0, _InputDir.x,Space.World);
+        
+    }
+}
