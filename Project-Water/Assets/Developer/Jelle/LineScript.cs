8a2817cb2abd2d29c775bd33d611ec75bfce5a5d
Unknown <jellesimonvds@gmail.com>
2018-11-28 22:32:58 +0100

    Verder gewerkt aan touwen

--- Project-Water/Assets/Developer/Jelle/LineScript.cs
+++ Project-Water/Assets/Developer/Jelle/LineScript.cs
@@ -22,5 +22,14 @@ public class LineScript : MonoBehaviour
     {
         Lr.SetPosition(0, new Vector3(transform.position.x, transform.position.y, transform.position.z));
         Lr.SetPosition(1, new Vector3(secondPoint.transform.position.x, secondPoint.transform.position.y, secondPoint.transform.position.z));
+        float dist = Vector3.Distance(transform.position, secondPoint.transform.position);
+        if(dist >= 5.5f)
+        {
+            Lr.enabled = false;
+        }
+        else
+        {
+            Lr.enabled = true;
+        }
     }
 }
