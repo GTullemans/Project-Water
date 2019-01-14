2e42b53023b5ca07e9297491532e7cb6b3c37861
TheDerpyDeed <36233010+TheDerpyDeed@users.noreply.github.com>
2019-01-14 12:20:00 +0100

    Map

---
+++ Project-Water/Assets/Artists/Rosa/Scripts/WaterTide.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterTide : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    float speed = 0.5f;
+    float height = 0.1f;
+
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        float newY = Mathf.Sin(Time.time * speed);
+        transform.position = new Vector3(pos.x, newY, pos.z) * height;
+    }
+
+}
