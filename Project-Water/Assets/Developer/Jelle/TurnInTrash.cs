0826f641d5decae1c0ba93a81ea7a65d617aa15a
Unknown <jellesimonvds@gmail.com>
2019-01-27 01:55:22 +0100

    trash inleveren script af

--- Project-Water/Assets/Developer/Jelle/TurnInTrash.cs
+++ Project-Water/Assets/Developer/Jelle/TurnInTrash.cs
@@ -10,7 +10,10 @@ public class TurnInTrash : MonoBehaviour
 
     private int[] _PlayerScore = new int[4];
     private bool[] PlayerEntered = new bool[4];
+    private bool GiveScoreBool;
+    private bool DeleteTrash;
     private int TrashCount;
+    private int BoatCount;
     void Start()
     {
 
@@ -18,7 +21,16 @@ public class TurnInTrash : MonoBehaviour
 
     void Update()
     {
-        print(PlayerEntered[0]);
+        if(BoatCount == 2)
+        {
+            Invoke("GiveScore", 1f);
+        }
+        else
+        {
+
+        }
+       // print(BoatCount);
+        //print(TrashCount);
         m_PlayerScoreText[0].text = "Player 1: " + _PlayerScore[0];
         m_PlayerScoreText[1].text = "Player 1: " + _PlayerScore[1];
         m_PlayerScoreText[2].text = "Player 1: " + _PlayerScore[2];
@@ -29,22 +41,28 @@ public class TurnInTrash : MonoBehaviour
         if (other.gameObject.name == "Boat1")
         {
             PlayerEntered[0] = true;
+            BoatCount += 1;
         }
         if (other.gameObject.name == "Boat2")
         {
             PlayerEntered[1] = true;
+            BoatCount += 1;
         }
         if (other.gameObject.name == "Boat3")
         {
             PlayerEntered[2] = true;
+            BoatCount += 1;
         }
         if (other.gameObject.name == "Boat4")
         {
             PlayerEntered[3] = true;
+            BoatCount += 1;
         }
-        if (other.CompareTag("Trash"))
+        if (other.gameObject.tag == "Trash")
         {
             TrashCount += 1;
+            other.gameObject.SetActive(false);
+            //GiveScore();
         }
     }
     private void OnTriggerExit(Collider other)
@@ -52,18 +70,26 @@ public class TurnInTrash : MonoBehaviour
         if (other.gameObject.name == "Boat1")
         {
             PlayerEntered[0] = false;
+            BoatCount -= 1;
         }
         if (other.gameObject.name == "Boat2")
         {
             PlayerEntered[1] = false;
+            BoatCount -= 1;
         }
         if (other.gameObject.name == "Boat3")
         {
             PlayerEntered[2] = false;
+            BoatCount -= 1;
         }
         if (other.gameObject.name == "Boat4")
         {
             PlayerEntered[3] = false;
+            BoatCount -= 1;
+        }
+        if(other.gameObject.tag == "Trash")
+        {
+            TrashCount -= 1;
         }
     }
     private void GiveScore()
@@ -73,8 +99,9 @@ public class TurnInTrash : MonoBehaviour
             if (PlayerEntered[i])
             {
                 _PlayerScore[i] += TrashCount / 2;
+                //PlayerEntered[i] = false;
             }
         }
-
+        TrashCount = 0;
     }
 }
